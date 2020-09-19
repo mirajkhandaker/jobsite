ca657c0e62c89f6a2c8689122072a527794aa1c8
Miraj Khandaker <mirajkhandaker@gmail.com>
2020-09-20 00:42:40 +0600

    first push

---
+++ app/Http/Controllers/EmployeerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Job;
+use Illuminate\Http\Request;
+
+class EmployeerController extends Controller
+{
+    public function dashboard(){
+        $jobs = Job::select('id','job_title')
+            ->with('applyJobs','applyJobs.applicants','applyJobs.applicants.resume')
+            ->paginate(20);
+        return view('employeer.dashboard',compact('jobs'));
+    }
+}
